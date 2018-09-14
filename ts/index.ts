df76935a526082ef33cf393a8a7c22e6d8526012
Phil Kunz <phil@lossless.digital>
2018-09-14 21:02:17 +0000

    Master

--- ts/index.ts
+++ ts/index.ts
@@ -1,29 +1,29 @@
-import * as handlebars from 'handlebars'
+import * as handlebars from 'handlebars';
 
 /**
  * class Tlt allows templates to be used with different sets of data
  */
 export class Tlt {
-  template: any
+  template: any;
 
   /**
    * constructor of class Tlt
    */
   constructor(templateStringArg: string) {
-    this.template = handlebars.compile(templateStringArg)
+    this.template = handlebars.compile(templateStringArg);
   }
 
   /**
    * returns template string with data applied
    */
   applyData(data: any): string {
-    return this.template(data)
+    return this.template(data);
   }
 
   /**
    * set a new template string
    */
   setTemplate(templateStringArg: string) {
-    this.template = handlebars.compile(templateStringArg)
+    this.template = handlebars.compile(templateStringArg);
   }
 }
