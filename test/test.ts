1ab343232503208cf521b443ea0542e633cf0059
Phil Kunz <gitlab@philkunz.com>
2021-03-27 15:07:02 +0000

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -12,7 +12,7 @@ tap.test('should create a valid instance of tlt', async () => {
 tap.test('should output a valid string with some data', async () => {
   let appliedString = testMustache.applyData({
     customString: 'horse',
-    license: 'MIT'
+    license: 'MIT',
   });
   expect(appliedString).to.equal('some awesome horse that is MIT licensed');
 });
